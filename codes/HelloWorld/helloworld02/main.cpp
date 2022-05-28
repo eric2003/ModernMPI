e6875847a1c1851904021fc0de108f4875d534f3
eric <fantasy_2003_@hotmail.com>
2022-05-28 16:32:42 +0800

    add readme

--- codes/HelloWorld/helloworld02/main.cpp
+++ codes/HelloWorld/helloworld02/main.cpp
@@ -10,8 +10,8 @@ int main(int argc, char* argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Get_library_version(version, &len);
-	std::cout << "Hello, world!  I am " << rank << " of " << size
-              << "(" << version << ", " << len << ")" << std::endl;		   
+    std::cout << "Hello, world!  I am " << rank << " of " << size
+              << "(" << version << ", " << len << ")" << std::endl;
     MPI_Finalize();
 
     return 0;
---
+++ codes/HelloWorld/helloworld02/readme.txt
@@ -0,0 +1,5 @@
+cmd.exe "/K" '"C:\Program Files (x86)\Intel\oneAPI\setvars.bat" && powershell'
+cmake -DCMAKE_C_COMPILER=icx -DCMAKE_CXX_COMPILER=icx -DCMAKE_BUILD_TYPE=Release -GNinja ..
+mpiexec -n 4 .\testprj.exe
+
+
